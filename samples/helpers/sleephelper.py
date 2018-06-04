0ea6601d04d294f0cb2d2bc6a3798d57dd16109d
ajnarayan <anjjan.narayan@yahoo.in>
2018-06-03 21:39:51 -0400

    Memoization

---
+++ samples/helpers/sleephelper.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+def sleep_decorator(function):
+
+    """
+    Limits how fast the function is
+    called.
+    """
+
+    def wrapper(*args, **kwargs):
+        sleep(2)
+        return function(*args, **kwargs)
+    return wrapper
