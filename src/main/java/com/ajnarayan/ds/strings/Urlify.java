2a0b320e04882677b84f2d2e2b84db56db8d9c97
ajnarayan <anjjan.narayan@yahoo.in>
2018-10-10 06:59:28 -0400

    Urlify Refactor

--- src/main/java/com/ajnarayan/ds/strings/Urlify.java
+++ src/main/java/com/ajnarayan/ds/strings/Urlify.java
@@ -10,27 +10,36 @@ public class Urlify {
         String str = "Mr John Smith     ";
         int trueLength = 13;
         System.out.println(str.length());
-        str = urlify(str.toCharArray(), trueLength);
-        System.out.println(" The new String is : " + str);
-        System.out.println(str.length());
+        char[] strChar = urlify(str.toCharArray(), trueLength);
+        System.out.println(" The new String is : \n");
+        for(int i=0 ; i<strChar.length; i++) {
+            System.out.print(strChar[i]);
+        }
+        System.out.println("\n" + strChar.length);
     }
 
     /**
      * Time: O(n)
-     * Space: O(1)
+     * Space: O(1) - Inplace since not using string
      * @param str
      * @param trueLength
      */
-    private static String urlify(char[] str, int trueLength) {
+    private static char[] urlify(char[] str, int trueLength) {
         int index, spacecount = 0;
+        //count the number of spaces
         for(int i =0 ; i< trueLength; i++){
             if(str[i] == ' '){
                 spacecount++;
             }
         }
 
+        //the new length to start filling from reverse
         index = trueLength + spacecount * 2;
 
+        //append \0 to indicate the end
+        if(trueLength< spacecount){
+            str[index--] = '\0';
+        }
         for(int i=trueLength-1; i>0 ; i--){
             if( str[i] == ' '){
                 str[index - 1] = '0';
@@ -43,6 +52,6 @@ public class Urlify {
             }
         }
 
-        return new String(str);
+        return str;
     }
 }
